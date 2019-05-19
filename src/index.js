d71471a16b928112871dc3c6fbc1fd2c32c1fee2
quilicicf <cquilici@restlet.com>
2019-05-19 16:20:29 +0200

    :new: Create page header

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import popper from 'popper.js';
+import bootstrap from 'bootstrap';
+
+
+import './sass/style.scss';
+
+window.$ = $;
+window.popper = popper;
+window.bootstrap = bootstrap;
