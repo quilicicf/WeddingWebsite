6432676e4358fd75de4e03d55833a62ee0bfed49
quilicicf <cquilici@restlet.com>
2019-10-26 17:32:49 +0200

    :art: Styling round I

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import $ from 'jquery';
 import popper from 'popper.js';
 import bootstrap from 'bootstrap';
-
+import util from 'bootstrap/js/dist/util';
+import carousel from 'bootstrap/js/dist/carousel';
 
 import './sass/style.scss';
 
 window.$ = $;
 window.popper = popper;
 window.bootstrap = bootstrap;
+window.util = util;
+window.carousel = carousel;
